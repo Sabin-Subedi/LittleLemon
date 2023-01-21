bff82e62eb45b02d433d10633e2c5cd50f9c71f2
Sabin Subedi <sabinsubedi125@gmail.com>
2023-01-21 10:59:38 +0545

    added the booking and menu model

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restaurant'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,14 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'OPTIONS':{
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Menu, Booking
+
+@admin.register(Menu)
+class Menu(admin.ModelAdmin):
+    list_display = ('id','title', 'price', 'inventory')
+
+@admin.register(Booking)
+class Booking(admin.ModelAdmin):
+    list_display = ('id','name', 'no_of_guests', 'booking_date')
