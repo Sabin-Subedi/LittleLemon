494c937de2b34e9f6ca9182a4593acb019492121
Sabin Subedi <sabinsubedi125@gmail.com>
2023-01-21 14:28:08 +0545

    added the token auth

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+     'rest_framework.authtoken',
+    'djoser',
     'restaurant'
 ]
 
@@ -134,3 +136,14 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+
+    ),
+}
+
+DJOSER={
+    'USER_ID_FIELD': 'username',
+}
